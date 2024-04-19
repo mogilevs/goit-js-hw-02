2f3d258c3c63792f003cd7ac69d93897e8a7ee05
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-04-19 10:00:30 +0300

    do task2

---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+function formatMessage(message, maxLength) {
+    if (message <= maxLength) {
+        return message;
+    } else {
+        let endIndex = maxLength.length - 1;
+        let newMessage = message.slice(0, endIndex) + "...";
+        return newMessage;
+    };
+    
+}
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
