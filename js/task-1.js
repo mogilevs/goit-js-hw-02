21e4a66da449f297fb979bd197b89bb53b488d1f
Oleh Mogylevsky <159718895+mogilevs@users.noreply.github.com>
2024-04-19 09:35:17 +0300

    do task1

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+makeTransaction(quantity, pricePerDroid, customerCredits){
+    let totalPrice = quantity * pricePerDroid;
+    if (totalPrice < customerCredits) {
+        console.log("Insufficient funds!");
+    } else {
+        console.log("You ordered ${quantity} droids worth ${totalPrice} credits!");
+    };
+}
